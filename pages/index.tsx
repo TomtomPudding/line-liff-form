ad488712683abb6ad6b8c1262243fd03fa8ac254
TomtomPudding <tomtomhobby1125@gmail.com>
2023-02-12 21:48:07 +0900

    add: click actions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
+import Button from "@mui/material/Button";
 import Head from "next/head";
-import packageJson from "../package.json";
 
 export default function Home() {
   return (
@@ -18,49 +18,38 @@ export default function Home() {
               LIFF Starter!
             </a>
           </h1>
-          <div className="home__badges">
-            <span className="home__badges__badge badge--primary">
-              LIFF Starter
-            </span>
-            <span className="home__badges__badge badge--secondary">nextjs</span>
-            <span className="home__badges__badge badge--primary">
-              {packageJson.version}
-            </span>
-            <a
-              href="https://github.com/line/line-liff-v2-starter"
-              target="_blank"
-              rel="noreferrer"
-              className="home__badges__badge badge--secondary"
-            >
-              GitHub
-            </a>
-          </div>
-          <div className="home__buttons">
-            <a
-              href="https://developers.line.biz/en/docs/liff/developing-liff-apps/"
-              target="_blank"
-              rel="noreferrer"
-              className="home__buttons__button button--primary"
-            >
-              LIFF Documentation
-            </a>
-            <a
-              href="https://liff-playground.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-              className="home__buttons__button button--tertiary"
-            >
-              LIFF Playground
-            </a>
-            <a
-              href="https://developers.line.biz/console/"
-              target="_blank"
-              rel="noreferrer"
-              className="home__buttons__button button--secondary"
-            >
-              LINE Developers Console
-            </a>
-          </div>
+          <Button
+            variant="contained"
+            onClick={(e) => {
+              import("@line/liff").then((liff: any) => {
+                liff
+                  .sendMessages([
+                    {
+                      type: "text",
+                      text: "Hello, World!",
+                    },
+                  ])
+                  .then(() => {
+                    console.log("message sent");
+                  })
+                  .catch((err: any) => {
+                    console.log("error", err);
+                  });
+              });
+            }}
+          >
+            TestMessage
+          </Button>
+          <Button
+            variant="contained"
+            onClick={(e) => {
+              import("@line/liff").then((liff: any) => {
+                liff.closeWindow();
+              });
+            }}
+          >
+            CloseLiff
+          </Button>
         </div>
       </div>
     </>
